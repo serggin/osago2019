7cd0989895bdcb19192a2124df86bbc84a8d44ba
okatana <okatana17@gmail.com>
2019-02-14 12:00:06 +0300

    index.js переименован в index.jsx

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require ('path')
 const webpack = require('webpack')
 module.exports = {
-    entry: './src/index.js',
+    entry: './src/index.jsx',
     mode: 'development',
     output: {
         path: path.resolve('public'),
